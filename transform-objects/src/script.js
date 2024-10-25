9cb40aa0edb00d1b88332d05b9c41650f7e6c321
shubham <shubhamverma1091@gmail.com>
2024-10-25 15:28:38 +0530

    transform object finished

--- transform-objects/src/script.js
+++ transform-objects/src/script.js
@@ -9,16 +9,70 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// // position
+// // mesh.position.x = 0.7;
+// // mesh.position.y = -0.6;
+// // mesh.position.z = 3;
+// mesh.position.set(0.7, -0.6, 1);
+
+// // mesh.position.normalize();
+// console.log(mesh.position.length());
+
+// // scale
+// // mesh.scale.x = 2;
+// // mesh.scale.y = 0.5;
+// // mesh.scale.z = 0.5;
+// mesh.scale.set(2, 0.5, 0.5);
+
+// // rotation
+// // euler rotation
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25; //Math.PI ==half a turn
+// mesh.rotation.x = Math.PI * 0.25;
+
+/**
+ * Group
+ */
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+
+group.add(cube1, cube2, cube3);
+cube1.position.set(-1, 0, 0);
+cube3.position.set(1, 0, 0);
+
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+
+/**
+ * Axes helper
+ */
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Sizes
  */
 const sizes = {
-  width: 800, 
+  width: 800,
   height: 600,
 };
 
@@ -26,9 +80,11 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.set(0, 0, 3);
 scene.add(camera);
 
+// console.log(mesh.position.distanceTo(new THREE.Vector3(...camera.position)));
+// camera.lookAt(mesh.position);
 /**
  * Renderer
  */
