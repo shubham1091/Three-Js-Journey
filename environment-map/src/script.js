39898f3a8d2e35ee05f015d429273f81e5737faa
shubham <shubhamverma1091@gmail.com>
2024-11-10 15:17:53 +0530

    evironmet map finished

--- environment-map/src/script.js
+++ environment-map/src/script.js
@@ -1,12 +1,24 @@
 import GUI from "lil-gui";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+
+/**
+ * Loaders
+ */
+
+const gltflLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const rgbeLoader = new RGBELoader();
+const textureLoader = new THREE.TextureLoader();
 
 /**
  * Base
  */
 // Debug
 const gui = new GUI();
+const global = {};
 
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -15,14 +27,96 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
+ * Update all materials
+ */
+
+const updateMaterials = () => {
+  scene.traverse((child) => {
+    // console.log(child);
+    if (child.isMesh && child.material instanceof THREE.MeshStandardMaterial) {
+      // console.log(child);
+      child.material.envMapIntensity = global.envMapIntensity;
+    }
+  });
+};
+
+/**
+ * Environment map
+ */
+scene.backgroundBlurriness = 0;
+scene.backgroundIntensity = 1;
+
+gui.add(scene, "backgroundBlurriness", 0, 1, 0.01);
+gui.add(scene, "backgroundIntensity", 0, 10, 0.01);
+// LDR cube texture
+let envIdx = 0;
+global.envMapIntensity = 1;
+gui.add(global, "envMapIntensity", 0, 10, 0.01).onChange(() => {
+  // envIdx = Math.floor(value);
+  updateMaterials();
+});
+// const envMap = cubeTextureLoader.load([
+//   `/environmentMaps/${envIdx}/px.png`,
+//   `/environmentMaps/${envIdx}/nx.png`,
+//   `/environmentMaps/${envIdx}/py.png`,
+//   `/environmentMaps/${envIdx}/ny.png`,
+//   `/environmentMaps/${envIdx}/pz.png`,
+//   `/environmentMaps/${envIdx}/nz.png`,
+// ]);
+
+// scene.background = envMap;
+// scene.environment = envMap;
+
+// HDR (rgbe) equirectangular environment map
+
+// rgbeLoader.load(`/environmentMaps/${envIdx}/2k.hdr`, (en) => {
+//   // console.log(en);
+//   en.mapping = THREE.EquirectangularReflectionMapping;
+//   scene.background = en;
+//   scene.environment = en;
+//   // updateMaterials();
+// });
+// rgbeLoader.load(`/environmentMaps/blockadesLabsSkybox/anime_art_style_japan_streets_with_cherry_blossom_.jpg`, (en) => {
+//   // console.log(en);
+//   en.mapping = THREE.EquirectangularReflectionMapping;
+//   // scene.background = en;
+//   scene.environment = en;
+//   // updateMaterials();
+// });
+
+// LDR equirectangular environment map
+
+const envMap = textureLoader.load(`/environmentMaps/blockadesLabsSkybox/anime_art_style_japan_streets_with_cherry_blossom_.jpg`)
+envMap.mapping = THREE.EquirectangularReflectionMapping;
+envMap.colorSpace = THREE.SRGBColorSpace;
+scene.background = envMap;
+scene.environment = envMap;
+
+/**
  * Torus Knot
  */
 const torusKnot = new THREE.Mesh(
   new THREE.TorusKnotGeometry(1, 0.4, 100, 16),
-  new THREE.MeshBasicMaterial()
+  new THREE.MeshStandardMaterial({
+    roughness: 0.3,
+    metalness: 1,
+    color: 0xaaaaaaa,
+  })
 );
 torusKnot.position.y = 4;
+torusKnot.position.x = -4;
 scene.add(torusKnot);
+
+/**
+ * Models
+ */
+gltflLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
+  // console.log("loaded");
+
+  gltf.scene.scale.set(10, 10, 10);
+  scene.add(gltf.scene);
+  updateMaterials();
+});
 
 /**
  * Sizes
