0c5aa7430297beabdbcd86575beb35531d2d03ac
shubham <shubhamverma1091@gmail.com>
2024-12-04 14:55:42 +0530

    realistic-render finished

--- realistic-render/src/script.js
+++ realistic-render/src/script.js
@@ -10,6 +10,8 @@ import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 const gltfLoader = new GLTFLoader();
 const rgbeLoader = new RGBELoader();
 
+const textureLoader = new THREE.TextureLoader();
+
 /**
  * Base
  */
@@ -30,6 +32,9 @@ const updateAllMaterials = () => {
   scene.traverse((child) => {
     if (child.isMesh && child.material.isMeshStandardMaterial) {
       child.material.envMapIntensity = global.envMapIntensity;
+
+      child.castShadow = true;
+      child.receiveShadow = true;
     }
   });
 };
@@ -54,6 +59,40 @@ rgbeLoader.load("/environmentMaps/0/2k.hdr", (environmentMap) => {
   scene.environment = environmentMap;
 });
 
+// directional light
+const directionalLight = new THREE.DirectionalLight("#fff", 1);
+directionalLight.position.set(-1, 6.5, 2.5);
+scene.add(directionalLight);
+
+gui
+  .add(directionalLight, "intensity", 0, 10)
+  .step(0.01)
+  .name("Light Intensity");
+
+gui.add(directionalLight.position, "x", -10, 10).step(0.01).name("light x");
+gui.add(directionalLight.position, "y", -10, 10).step(0.01).name("light y");
+gui.add(directionalLight.position, "z", -10, 10).step(0.01).name("light z");
+
+// shadows
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(512, 512);
+gui.add(directionalLight, "castShadow").name("Cast Shadow");
+
+// helper
+const directionalLightHelper = new THREE.CameraHelper(
+  directionalLight.shadow.camera
+);
+scene.add(directionalLightHelper);
+
+// Target
+directionalLight.target.position.set(0, 4, 0);
+directionalLight.target.updateWorldMatrix();
+
+directionalLightHelper.visible = false;
+
+gui.add(directionalLightHelper, "visible").name("Directional Light Helper");
+
 /**
  * Models
  */
@@ -64,6 +103,52 @@ gltfLoader.load("/models/FlightHelmet/glTF/FlightHelmet.gltf", (gltf) => {
 
   updateAllMaterials();
 });
+
+
+/**
+ * Floor
+ */
+const floorColorTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_diff_1k.jpg')
+const floorNormalTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_nor_gl_1k.png')
+const floorAORoughnessMetalnessTexture = textureLoader.load('/textures/wood_cabinet_worn_long/wood_cabinet_worn_long_arm_1k.jpg')
+
+floorColorTexture.colorSpace = THREE.SRGBColorSpace
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 8),
+    new THREE.MeshStandardMaterial({
+        map: floorColorTexture,
+        normalMap: floorNormalTexture,
+        aoMap: floorAORoughnessMetalnessTexture,
+        roughnessMap: floorAORoughnessMetalnessTexture,
+        metalnessMap: floorAORoughnessMetalnessTexture,
+    })
+)
+floor.rotation.x = - Math.PI * 0.5
+scene.add(floor)
+
+/**
+ * Wall
+ */
+const wallColorTexture = textureLoader.load('/textures/castle_brick_broken_06/castle_brick_broken_06_diff_1k.jpg')
+const wallNormalTexture = textureLoader.load('/textures/castle_brick_broken_06/castle_brick_broken_06_nor_gl_1k.png')
+const wallAORoughnessMetalnessTexture = textureLoader.load('/textures/castle_brick_broken_06/castle_brick_broken_06_arm_1k.jpg')
+
+wallColorTexture.colorSpace = THREE.SRGBColorSpace
+
+const wall = new THREE.Mesh(
+    new THREE.PlaneGeometry(8, 8),
+    new THREE.MeshStandardMaterial({
+        map: wallColorTexture,
+        normalMap: wallNormalTexture,
+        aoMap: wallAORoughnessMetalnessTexture,
+        roughnessMap: wallAORoughnessMetalnessTexture,
+        metalnessMap: wallAORoughnessMetalnessTexture,
+    })
+)
+wall.position.y = 4
+wall.position.z = - 4
+scene.add(wall)
 
 /**
  * Sizes
@@ -110,9 +195,29 @@ controls.enableDamping = true;
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Tone mapping
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 3;
+
+gui.add(renderer, "toneMapping", {
+  No: THREE.NoToneMapping,
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping,
+});
+
+gui.add(renderer, "toneMappingExposure").min(0).max(10).step(0.01);
+
+// shadows
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.shadowMap.autoUpdate = true;
 
 /**
  * Animate
